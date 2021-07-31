b4b3469a16486ce363f87e3499a0962132de4dbe
Alex Vong <avong003@gmail.com>
2021-07-30 23:23:33 -0700

    menu

---
+++ modules/site-menu.php
@@ -0,0 +1,22 @@
+
+
+<nav class="site-menu">
+	<inner-column>
+		<div class="site-title">
+			<a href='?page=home'>
+				<picture>
+					<img src="images/pokedex-logo.png" alt="">
+				</picture>
+			</a>
+		</div>
+
+		<div class="site-nav">
+	 		<a href='?page=home'>Home</a>
+			<a href='?page=kanto'>Kanto</a>
+			<a href='?page=johto'>Johto</a>
+			<a href='?page=hoenn'>Hoenn</a> 			
+		</div>
+
+	</inner-column>
+
+</nav>
