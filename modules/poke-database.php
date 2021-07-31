b195dce45a487823f0bbbf97c76475b0211aa552
Alex Vong <avong003@gmail.com>
2021-07-30 23:23:29 -0700

    database

---
+++ modules/poke-database.php
@@ -0,0 +1,95 @@
+<?php 
+
+
+$kanto = [
+
+	[
+		'id' => 001,
+		'name' => 'Bulbasaur',
+		'type' => 'Grass',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+		'description' => 'Bulbasaur has a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.'
+	],
+
+	[
+		'id' => 007,
+		'name' => 'Charmander',
+		'type' => 'Fire',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+		'description' => "A fire burns at the tip of this Pokémon's slender tail and has blazed there since Charmander's birth. The flame can be used as an indication of Charmander's health and mood, burning brightly when the Pokémon is strong, weakly when it is exhausted, wavering when it is happy, and blazing when it is enraged. It is said that Charmander dies if its flame goes out. However, if the Pokémon is healthy, the flame will continue to burn even if it gets a bit wet and is said to steam in the rain."
+	],
+
+	[
+		'id' => 004,
+		'name' => 'Squirtle',
+		'type' => 'Water',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+		'description' => 'Squirtle is a small Pokémon that resembles a light-blue turtle. Its body is encased by a tough shell that forms and hardens after birth. When Squirtle retracts its long neck into its shell, it squirts out water with vigorous force. '
+
+	],
+
+
+
+	
+
+];
+
+$johto = [
+
+	[
+		'id' => 153,
+		'name' => 'Chikorita',
+		'type' => 'Grass',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/152.png',
+		'description' => 'In battle, Chikorita waves its leaf around to keep the foe at bay. However, a sweet fragrance also wafts from the leaf, becalming the battling Pokémon and creating a cozy, friendly atmosphere all around. '
+	],
+
+	[
+		'id' => 155,
+		'name' => 'Cyndaquil',
+		'type' => 'Fire',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png',
+		'description' => 'Cyndaquil protects itself by flaring up the flames on its back. The flames are vigorous if the Pokémon is angry. However, if it is tired, the flames splutter fitfully with incomplete combustion. '
+	],
+
+
+	[
+		'id' => 158,
+		'name' => 'Totodile',
+		'type' => 'Water',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/158.png',
+		'description' => 'Despite the smallness of its body, Totodile’s jaws are very powerful. While the Pokémon may think it is just playfully nipping, its bite has enough power to cause serious injury. '
+	],
+
+
+];
+
+$hoenn = [
+
+	[
+		'id' => 252,
+		'name' => 'Treecko',
+		'type' => 'Grass',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/252.png',
+		'description' => 'Treecko has small hooks on the bottom of its feet that enable it to scale vertical walls. This Pokémon attacks by slamming foes with its thick tail.'
+	],
+
+	[
+		'id' => 255,
+		'name' => 'Torchic',
+		'type' => 'Fire',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/255.png',
+		'description' => 'Torchic sticks with its Trainer, following behind with unsteady steps. This Pokémon breathes fire of over 1,800 degrees Fahrenheit, including fireballs that leave the foe scorched black. '
+	],
+
+	[
+		'id' => 258,
+		'name' => 'Mudkip',
+		'type' => 'Water',
+		'image' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png',
+		'description' => 'The fin on Mudkip’s head acts as highly sensitive radar. Using this fin to sense movements of water and air, this Pokémon can determine what is taking place around it without using its eyes. '
+	]
+
+
+
+];
