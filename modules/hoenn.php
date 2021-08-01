cd0aadf2ad0ba2fe8b824bbdef32fec651d7aeee
Alex Vong <avong003@gmail.com>
2021-07-31 23:11:40 -0700

    hoenn

--- modules/hoenn.php
+++ modules/hoenn.php
@@ -1,13 +1,15 @@
-<h1>Hoenn Region</h1>
+
+
 
   <?php include('poke-database.php'); ?>
       
   <inner-column>
+  <h1 class='loud-voice'>Hoenn Region</h1>
   <ol class='poke-container'>
 
 	  <?php foreach($hoenn as $pokemon) { 
-	     include('poke-card.php');
-	   } ?>
+	     include('modules/poke-card.php');
+	  } ?>
 
 	</ol>
 	<inner-column>
