7a07749f88542b01a936e390610d19e3c26e356a
Alex Vong <avong003@gmail.com>
2021-07-31 23:11:15 -0700

    home

--- home.php
+++ home.php
@@ -1,21 +1,25 @@
-<h1 class='welcome'>Welcome to the exciting world of Pokemon!</h1>
+
+<h1 class='welcome loud-voice'>Welcome to the exciting world of Pokemon!</h1>
 
 <section class="regions">
 	<inner-column>
-		<h2>Search By Region:</h2>
+		<h2 class='search-heading medium-voice'>Search By Region:</h2>
 
 		<div class="region-cards">
-				<div class="kanto card">
+				<div class="kanto region">
 						<a href='?page=kanto'>Kanto</a>
 				</div>				
 	
-			<div class="johto card">
+			<div class="johto region">
 					<a href='?page=johto'>Johto</a>
 			</div>
 
-			<div class="hoenn card">
+			<div class="hoenn region">
 					<a href='?page=hoenn'>Hoenn</a>
 			</div>
 		</div>
-</inner-column>
+	</inner-column>
 </section>
+
+
+
