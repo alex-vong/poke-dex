f0caf35e39f4d8ea18925cc2998e9a6ac7eeff87
Alex Vong <avong003@gmail.com>
2021-08-02 15:05:41 -0700

    add 'under construction' to johnto

--- modules/johto.php
+++ modules/johto.php
@@ -5,6 +5,7 @@
       
   <inner-column>
   <h1 class='loud-voice'>Johto Region</h1>
+  <h2 class="loud-voice">Under Construction</h2>
   <ol class='poke-container'>
 
 	  <?php foreach($johto as $pokemon) { 
