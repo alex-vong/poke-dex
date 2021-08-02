0702485a0d56b0865dd78e0daa62449dd07d9e34
Alex Vong <avong003@gmail.com>
2021-08-02 15:04:49 -0700

    removed router from index

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <!DOCTYPE html>
 
-<?php 
-	$page = null; //setting page to a value that does not exist yet
+<?php require 'modules/router.php';?>
 
-	if ( isset($_GET['page'])) { 
-		$page = $_GET['page']; //if page is set then assign it value to $page
-	} else {
-		$page = 'home'; //if not set then default $page to 'home'
-	}
-?>
 <html lang="en">
-
-<!-- 
-pages: home, kanto list, johto list, hoenn list -->
 
 <head>
 	<?php include 'head.php' ?>
@@ -33,14 +23,17 @@ pages: home, kanto list, johto list, hoenn list -->
 			if ($page == 'kanto') {
 				include 'modules/kanto.php';
 			}
-			if ($page == 'johto') {
-				include 'modules/johto.php';
-			}
-			if ($page == 'hoenn') {
-				include 'modules/hoenn.php';
-			}
+			// if ($page == 'johto') {
+			// 	include 'modules/johto.php';
+			// }
+			// if ($page == 'hoenn') {
+			// 	include 'modules/hoenn.php';
+			// }
 			if ($page == 'detail') {
 				include 'modules/detail.php';
+			}
+			if ($page == 'create') {
+				include 'modules/create.php';
 			}
 	 ?>
 	</main>
