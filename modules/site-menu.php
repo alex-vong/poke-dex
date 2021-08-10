e483afb4447c81b8b30e7486ac002fc302c326ab
Alex Vong <avong003@gmail.com>
2021-08-10 11:09:52 -0700

    style

--- modules/site-menu.php
+++ modules/site-menu.php
@@ -2,6 +2,10 @@
 
 <nav class="site-menu">
 	<inner-column>
+		<div class="site-nav">
+	 		<a href='?page=home' class='home'>Home</a>
+			<a href='?page=create' class='create'>Add</a>	
+		</div>
 		<div class="site-title">
 			<a href='?page=home'>
 				<picture>
@@ -10,13 +14,7 @@
 			</a>
 		</div>
 
-		<div class="site-nav">
-	 		<a href='?page=home' class='home'>Home</a>
-			<a href='?page=kanto' class='kanto'>Kanto</a>
-<!-- 			<a href='?page=johto' class='johto'>Johto</a>
-			<a href='?page=hoenn' class='hoenn'>Hoenn</a> -->
-			<a href='?page=create' class='create'>Add</a>  			
-		</div>
+
 
 	</inner-column>
 
